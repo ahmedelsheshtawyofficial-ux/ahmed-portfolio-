4d45ba2e34e6558fdeac71eeed9ef5e9209d9207
ahmedelsheshtawyofficial-ux <ahmedelsheshtawyofficial@gmail.com>
2026-09-07 08:03:07 +0300

    Add files via upload

---
+++ migrations/0007_project_media_types.sql
@@ -0,0 +1,4 @@
+ALTER TABLE projects ADD COLUMN model_images TEXT DEFAULT '';
+ALTER TABLE projects ADD COLUMN dashboard_images TEXT DEFAULT '';
+ALTER TABLE projects ADD COLUMN model_files TEXT DEFAULT '';
+ALTER TABLE projects ADD COLUMN dashboard_files TEXT DEFAULT '';
